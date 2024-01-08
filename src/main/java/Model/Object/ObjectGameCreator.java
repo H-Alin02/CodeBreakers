779571849ab1c6140314c61f835b830eb5645782
Manda Hery Ny Aina <rakotomanda00@gmail.com>
2024-01-08 16:48:29 +0100

    modificazione sul ObjectGameCreator

--- src/main/java/Model/Object/ObjectGameCreator.java
+++ src/main/java/Model/Object/ObjectGameCreator.java
@@ -1,11 +1,6 @@
 package Model.Object;
 
-import java.lang.reflect.InvocationTargetException;
-
 public interface ObjectGameCreator {
     GameObject createObject(String nameObject,int posX, int posY);
 
-    public <T extends GameObject> GameObject
-    createObject(Class<T> type, int posX, int posY)
-            throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException;
 }
